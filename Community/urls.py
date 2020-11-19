97cb3d2e149af8472c9bd28287b9a90928bf629b
zngsm <popop09090@naver.com>
2020-11-19 17:57:11 +0900

    1119

--- Community/urls.py
+++ Community/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.review_list_create),
     path('<int:pk>/', views.review_update_delete),
-    path('<int:pk>/like/', views.like),
     path('<int:pk>/comment/', views.comment),
     path('<int:pk>/comment/<int:comment_pk>', views.comment_update_delete),
+    path('<int:pk>/like/', views.like),
 ]
