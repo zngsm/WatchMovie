71eed3aaf6923b0a664670c4511ec8be241f767a
dorosee97 <dk.myeong@gmail.com>
2020-11-21 15:22:11 +0900

    urls.py

--- Community/urls.py
+++ Community/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.review_list_create),
     path('<int:pk>/', views.review_update_delete),
     path('<int:pk>/comment/', views.comment),
-    path('<int:pk>/comment/<int:comment_pk>', views.comment_update_delete),
+    path('<int:pk>/comment/<int:comment_pk>/', views.comment_update_delete),
     # path('<int:pk>/like/', views.like),
 ]
